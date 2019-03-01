72932ba7bd6ef4aaec404023c40185cd4a26a8c4
Sunkyug Choi <eskei9981@gmail.com>
2019-03-01 12:17:02 +0900

    Create EmployeeNode.java

---
+++ src/abstract_data_types/linkedlists/challenge1/EmployeeNode.java
@@ -0,0 +1,41 @@
+package abstract_data_types.linkedlists.challenge1;
+
+public class EmployeeNode {
+
+    Employee employee;
+    EmployeeNode previous;
+    EmployeeNode next;
+
+    public EmployeeNode(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public EmployeeNode getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(EmployeeNode previous) {
+        this.previous = previous;
+    }
+
+    public EmployeeNode getNext() {
+        return next;
+    }
+
+    public void setNext(EmployeeNode next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return employee.toString();
+    }
+}
